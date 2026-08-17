efeab9f2318cf154ebe9462e95a45c6f3b371f37
ljuszczak <ljuszczak@rcoe.us>
2026-08-17 13:27:16 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // STARTER CONFIGURATION
 // Leave DEMO_MODE = true until your Supabase project is connected.
 window.COG_CONFIG = {
-  DEMO_MODE: true,
-  SUPABASE_URL: "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE",
+  DEMO_MODE: false,
+  SUPABASE_URL: "https://dhkpznivsonoyqmrxqjk.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_8X2eUlHA00XQNSmZsgFSIA_XXFZFSUC",
   MAX_COGS: 75
 };
